7a0b2f34db036ac09e54d1b5e904cd5d8ea4881c
6704062611018 <s6704062611018@email.kmutnb.ac.th>
2024-11-28 14:59:25 +0700

    complete4

--- lab1minx.c
+++ lab1minx.c
@@ -2,13 +2,7 @@
 int main() {
 	int num1;
 	int num2;
-	
-	printf("n1= ");
-	scanf("%d", &num1);
-	
-	printf("n2= ");
-	scanf("%d", &num2);
-	
+	scanf("%d %d", &num1, &num2);
 	if (num1>num2) {
 		int total = num1-num2;
 		printf("num1 greater than num2 = %d", total);	
