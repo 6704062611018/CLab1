8e1db3b3000967d8e7267f4b025be9014c61fadf
6704062611018 <s6704062611018@email.kmutnb.ac.th>
2024-11-28 14:53:47 +0700

    complete2

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 int main() {
-	int num1=5;
-	int num2=10;
+	int num1;
+	int num2;
+	
+	printf("n1= ");
+	scanf("%d", &num1);
+	
+	printf("n2= ");
+	scanf("%d", &num2);
+	
 	if (num1>num2) {
-		printf("num1 greater than num2");
-	} else {
-	
-	
-		printf("num2 greater than num1");
+		int total = num1-num2;
+		printf("num1 greater than num2 = %d", total);	
 	}
+	else {
+		int total = num2-num1;
+		printf ("num2 greater than num1 = %d", total);	
+	}	
 }
